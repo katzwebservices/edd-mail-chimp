f2698dd2d59b46022d31433269af143a7ddc1cf8
Pippin Williamson <pippin@pippinsplugins.com>
2013-08-13 21:07:07 -0500

    Complete rewrite including base newsletter class

--- edd-mailchimp.php
+++ edd-mailchimp.php
@@ -11,7 +11,7 @@ Contributors: Pippin Williamson
 
 define( 'EDD_MAILCHIMP_STORE_API_URL', 'https://easydigitaldownloads.com' );
 define( 'EDD_MAILCHIMP_PRODUCT_NAME', 'Mail Chimp' );
-
+define( 'EDD_MAILCHIMP_PATH', dirname( __FILE__ ) );
 
 /*
 |--------------------------------------------------------------------------
@@ -24,138 +24,10 @@ if( ! class_exists( 'EDD_License' ) ) {
 }
 $eddmc_license = new EDD_License( __FILE__, EDD_MAILCHIMP_PRODUCT_NAME, '1.0.9', 'Pippin Williamson' );
 
-/*
-|--------------------------------------------------------------------------
-| INTERNATIONALIZATION
-|--------------------------------------------------------------------------
-*/
 
-function eddmc_textdomain() {
-
-	// Set filter for plugin's languages directory
-	$edd_lang_dir = dirname( plugin_basename( __FILE__ ) ) . '/languages/';
-	$edd_lang_dir = apply_filters( 'eddmc_languages_directory', $edd_lang_dir );
-
-	// Load the translations
-	load_plugin_textdomain( 'eddmc', false, $edd_lang_dir );
+if( ! class_exists( 'EDD_Newsletter' ) ) {
+	include( dirname( __FILE__ ) . '/includes/class-edd-newsletter.php' );
 }
-add_action('init', 'eddmc_textdomain');
+include( dirname( __FILE__ ) . '/includes/class-edd-mailchimp.php' );
 
-
-// adds the settings to the Misc section
-function eddmc_add_settings($settings) {
-
-  $eddmc_settings = array(
-		array(
-			'id' => 'eddmc_settings',
-			'name' => '<strong>' . __('Mail Chimp Settings', 'eddmc') . '</strong>',
-			'desc' => __('Configure Mail Chimp Integration Settings', 'eddmc'),
-			'type' => 'header'
-		),
-		array(
-			'id' => 'eddmc_api',
-			'name' => __('Mail Chimp API Key', 'eddmc'),
-			'desc' => __('Enter your Mail Chimp API key', 'eddmc'),
-			'type' => 'text',
-			'size' => 'regular'
-		),
-		array(
-			'id' => 'eddmc_list',
-			'name' => __('Choose a list', 'edda'),
-			'desc' => __('Select the list you wish to subscribe buyers to', 'eddmc'),
-			'type' => 'select',
-			'options' => eddmc_get_mailchimp_lists()
-		),
-		array(
-			'id' => 'eddmc_label',
-			'name' => __('Checkout Label', 'eddmc'),
-			'desc' => __('This is the text shown next to the signup option', 'eddmc'),
-			'type' => 'text',
-			'size' => 'regular'
-		),
-		array(
-			'id' => 'eddmc_double_opt_in',
-			'name' => __('Double Opt-In', 'eddmc'),
-			'desc' => __('When checked, users will be sent a confirmation email after signing up, and will only be adding once they have confirmed the subscription.', 'eddmc'),
-			'type' => 'checkbox'
-		)
-	);
-
-	return array_merge($settings, $eddmc_settings);
-}
-add_filter('edd_settings_misc', 'eddmc_add_settings');
-
-
-// get an array of all mailchimp subscription lists
-function eddmc_get_mailchimp_lists() {
-
-	global $edd_options, $pagenow, $edd_settings_page;
-
-	if( ! isset( $_GET['page'] ) || ! isset( $_GET['tab'] ) || $_GET['page'] != 'edd-settings' || $_GET['tab'] != 'misc' )
-		return;
-	if( isset( $edd_options['eddmc_api'] ) && strlen( trim( $edd_options['eddmc_api'] ) ) > 0 ) {
-
-		if( !class_exists( 'MCAPI' ) )
-			require_once('mailchimp/MCAPI.class.php');
-
-		$lists = get_transient( 'sedd_mailchimp_lists' );
-		if( false === $lists ) {
-
-			$api = new MCAPI($edd_options['eddmc_api']);
-			$list_data = $api->lists();
-
-			if($list_data) :
-				foreach($list_data['data'] as $key => $list) :
-					$lists[$list['id']] = $list['name'];
-				endforeach;
-			endif;
-			set_transient( 'edd_mailchimp_lists', $lists, 24*24*24 );
-		}
-		return $lists;
-	}
-	return array();
-}
-
-// adds an email to the mailchimp subscription list
-function eddmc_subscribe_email( $user_info ) {
-	global $edd_options;
-
-	if( ! empty( $edd_options['eddmc_api'] ) ) {
-		if( !class_exists( 'MCAPI' ) )
-			require_once('mailchimp/MCAPI.class.php');
-		$api = new MCAPI( trim( $edd_options['eddmc_api'] ) );
-		$opt_in = isset($edd_options['eddmc_double_opt_in']) ? true : false;
-		if( $api->listSubscribe( $edd_options['eddmc_list'], $user_info['email'], array( 'FNAME' => $user_info['first_name'], 'LNAME' => $user_info['last_name'] ), 'html', $opt_in ) === true) {
-			return true;
-		}
-	}
-
-	return false;
-}
-
-// displays the mailchimp checkbox
-function eddmc_mailchimp_fields() {
-	global $edd_options;
-	$label = ! empty( $edd_options['eddmc_label'] ) ? $edd_options['eddmc_label'] : __( 'Sign up for our mailing list', 'eddmc' );
-	ob_start();
-		if( ! empty( $edd_options['eddmc_api'] ) ) { ?>
-		<fieldset id="edd_mailchimp">
-			<p>
-				<input name="eddmc_mailchimp_signup" id="eddmc_mailchimp_signup" type="checkbox" checked="checked"/>
-				<label for="eddmc_mailchimp_signup"><?php echo $label; ?></label>
-			</p>
-		</fieldset>
-		<?php
-	}
-	echo ob_get_clean();
-}
-add_action('edd_purchase_form_before_submit', 'eddmc_mailchimp_fields', 100);
-
-// checks whether a user should be signed up for he mailchimp list
-function eddmc_check_for_email_signup( $posted, $user_info, $valid_data ) {
-	if( isset( $posted['eddmc_mailchimp_signup'] ) ) {
-
-		eddmc_subscribe_email( $user_info );
-	}
-}
-add_action('edd_checkout_before_gateway', 'eddmc_check_for_email_signup', 10, 3 );
+$edd_mc = new EDD_MailChimp( 'mailchimp', 'Mail Chimp' );
---
+++ includes/class-edd-mailchimp.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Base newsletter class
+ *
+ * @copyright   Copyright (c) 2012, Pippin Williamson
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+*/
+
+class EDD_MailChimp extends EDD_Newsletter {
+
+
+	public function init() {
+		global $edd_options;
+		if( ! empty( $edd_options['eddmc_label'] ) ) {
+			$this->checkout_label = trim( $edd_options['eddmc_label'] );
+		} else {
+			$this->checkout_label = 'Signup for the newsletter';
+		}
+
+	}
+
+	public function get_lists() {
+
+		global $edd_options;
+
+		if( ! empty( $edd_options['eddmc_api'] ) ) {
+
+			if( ! class_exists( 'MCAPI' ) )
+				require_once( EDD_MAILCHIMP_PATH . '/mailchimp/MCAPI.class.php' );
+
+			$list_data = get_transient( 'edd_mailchimp_lists' );
+			if( false === $list_data ) {
+
+				$api = new MCAPI( $edd_options['eddmc_api'] );
+				$list_data = $api->lists();
+				set_transient( 'edd_mailchimp_lists', $list_data, 24*24*24 );
+			}
+			if( $list_data ) {
+				foreach( $list_data['data'] as $key => $list ) {
+
+					$this->lists[ $list['id'] ] = $list['name'];
+
+				}
+			}
+		}
+
+		return (array) $this->lists;
+	}
+
+	public function settings( $settings ) {
+
+		$eddmc_settings = array(
+			array(
+				'id'      => 'eddmc_settings',
+				'name'    => '<strong>' . __('Mail Chimp Settings', 'eddmc' ) . '</strong>',
+				'desc'    => __( 'Configure Mail Chimp Integration Settings', 'eddmc' ),
+				'type'    => 'header'
+			),
+			array(
+				'id'      => 'eddmc_api',
+				'name'    => __( 'Mail Chimp API Key', 'eddmc' ),
+				'desc'    => __( 'Enter your Mail Chimp API key', 'eddmc' ),
+				'type'    => 'text',
+				'size'    => 'regular'
+			),
+			array(
+				'id'      => 'eddmc_show_checkout_signup',
+				'name'    => __( 'Show Signup on Checkout', 'eddmc' ),
+				'desc'    => __( 'Allow customers to signup for the list selected below during checkout?', 'eddmc' ),
+				'type'    => 'checkbox'
+			),
+			array(
+				'id'      => 'eddmc_list',
+				'name'    => __( 'Choose a list', 'edda'),
+				'desc'    => __( 'Select the list you wish to subscribe buyers to', 'eddmc' ),
+				'type'    => 'select',
+				'options' => $this->get_lists()
+			),
+			array(
+				'id'      => 'eddmc_label',
+				'name'    => __( 'Checkout Label', 'eddmc' ),
+				'desc'    => __( 'This is the text shown next to the signup option', 'eddmc' ),
+				'type'    => 'text',
+				'size'    => 'regular'
+			),
+			array(
+				'id'      => 'eddmc_double_opt_in',
+				'name'    => __( 'Double Opt-In', 'eddmc' ),
+				'desc'    => __( 'When checked, users will be sent a confirmation email after signing up, and will only be adding once they have confirmed the subscription.', 'eddmc' ),
+				'type'    => 'checkbox'
+			)
+		);
+
+		return array_merge( $settings, $eddmc_settings );
+	}
+
+	public function show_checkout_signup() {
+		global $edd_options;
+
+		return ! empty( $edd_options['eddmc_show_checkout_signup'] );
+	}
+
+	public function subscribe_email( $user_info = array(), $list_id = false ) {
+
+		global $edd_options;
+
+		// Make sure an API key has been entered
+		if( empty( $edd_options['eddmc_api'] ) ) {
+			return false;
+		}
+
+		// Retrieve the global list ID if none is provided
+		if( ! $list_id ) {
+			$list_id = ! empty( $edd_options['eddmc_list'] ) ? $edd_options['eddmc_list'] : false;
+			if( ! $list_id ) {
+				return false;
+			}
+		}
+
+
+		if( ! class_exists( 'MCAPI' ) ) {
+			require_once( EDD_MAILCHIMP_PATH . '/mailchimp/MCAPI.class.php' );
+		}
+
+		$api    = new MCAPI( trim( $edd_options['eddmc_api'] ) );
+		$opt_in = isset( $edd_options['eddmc_double_opt_in'] );
+
+		if( $api->listSubscribe( $edd_options['eddmc_list'], $user_info['email'], array( 'FNAME' => $user_info['first_name'], 'LNAME' => $user_info['last_name'] ), 'html', $opt_in ) === true ) {
+			return true;
+		}
+
+		return false;
+
+	}
+
+}
